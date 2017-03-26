446f7c9a09376199e2a96ffc5014d358e641f583
Tom Hnatovsky <hnatovsky@xswd.cz>
2017-03-26 16:47:53 +0200

    Better configurability

--- src/mobile-navigation.js
+++ src/mobile-navigation.js
@@ -1,23 +1,31 @@
 /*! Shopeca MobileNavigation (0.0.1). (C) 2017 Tom Hnatovsky, Shopeca.com. BSD-3-Clause @license: https://opensource.org/licenses/BSD-3-Clause */
 
 ;var Shopeca = Shopeca || {}; Shopeca.MobileNavigation = {
+	settings: {
+		templates: {
+			button: '<button class="mobile-navigation-button"></button>',
+			panel: '<div class="mobile-navigation-panel"></div>',
+			wrapper: '<div class="mobile-navigation-wrapper"></div>',
+			heading: '<div class="mobile-navigation-heading"></div>',
+			sectionPanel: '<div class="mobile-navigation-section"></div>',
+			sectionButton: '<button></button>',
+			sectionButtonLine: '<div class="mobile-navigation-section-buttons"></div>'
+		},
+		classNames: {
+			buttonActive: 'active',
+			sectionActive: 'active',
+			sectionButtonActive: 'active'
+		}
+	},
 	inited: false,
 	initedContent: false,
 	active: false,
 	button: null,
-	buttonTemplate: '<button class="mobile-navigation-button hamburger hamburger--squeeze" type="button"><span class="hamburger-box"><span class="hamburger-inner"></span></span></button>',
 	panel: null,
-	panelTemplate: '<div class="mobile-navigation-panel"></div>',
 	wrapper: null,
-	wrapperTemplate: '<div class="mobile-navigation-wrapper"></div>',
 	heading: null,
-	headingTemplate: '<div class="mobile-navigation-heading"></div>',
-	settings: {},
 	sections: [],
 	sectionPanels: [],
-	sectionPanelTemplate: '<div class="mobile-navigation-section"></div>',
-	sectionButtonTemplate: '<button></button>',
-	sectionButtonLineTemplate: '<div class="mobile-navigation-section-buttons"></div>',
 	sectionButtons: [],
 	sectionNames: [],
 	activeSection: null,
@@ -29,18 +37,18 @@
 			this.button
 				.on('click', function(e){
 					e.preventDefault();
-					$(this).toggleClass('is-active');
+					$(this).toggleClass($this.settings.classNames.buttonActive);
 					$this.toggle();
 				})
 		}
 	},
 	initElements: function () {
-		this.button = $(this.buttonTemplate);
-		this.wrapper = $(this.wrapperTemplate);
-		this.panel = $(this.panelTemplate);
+		this.button = $(this.settings.templates.button);
+		this.wrapper = $(this.settings.templates.wrapper);
+		this.panel = $(this.settings.templates.panel);
 		this.panel.appendTo(this.wrapper);
 
-		this.heading = $(this.headingTemplate);
+		this.heading = $(this.settings.templates.heading);
 		this.heading.appendTo(this.panel);
 		if ('heading' in this.settings) {
 			this.heading.html(this.settings.heading);
@@ -62,15 +70,15 @@
 
 			if (this.sections.length > 0) {
 				var $this = this;
-				var line = $(this.sectionButtonLineTemplate);
+				var line = $(this.settings.templates.sectionButtonLine);
 				for (var i = 0; i < this.sections.length; i++) {
-					var panel = $(this.sectionPanelTemplate);
+					var panel = $(this.settings.templates.sectionPanel);
 					panel
 						.html(this.sections[i].clone())
 						.appendTo(this.panel);
 					this.sectionPanels.push(panel);
 
-					var button = $(this.sectionButtonTemplate);
+					var button = $(this.settings.templates.sectionButton);
 					button
 						.data('index', i)
 						.appendTo(line)
@@ -87,7 +95,7 @@
 		}
 	},
 	configure: function (settings) {
-		this.settings = settings;
+		this.settings = $.extend(true, this.settings, settings);
 		if ('jRes' in this.settings && 'breakpoint' in this.settings) {
 			var $this = this;
 			this.settings.jRes.addFunc({
@@ -112,7 +120,7 @@
 		this.init();
 		this.close(true);
 		this.button
-			.removeClass('is-active')
+			.removeClass(this.settings.classNames.buttonActive)
 			.hide();
 	},
 
@@ -145,15 +153,15 @@
 		}
 	},
 	switchSection: function (index) {
-		if (typeof this.sectionButtons[index] != 'undefined' && index !== this.activeSection) {
+		if (typeof this.sectionButtons[index] !== 'undefined' && index !== this.activeSection) {
 			for (var i = 0; i < this.sectionButtons.length; i++) {
 				if (i == index) {
 					this.activeSection = i;
-					this.sectionPanels[i].addClass('active');
-					this.sectionButtons[i].addClass('active');
+					this.sectionPanels[i].addClass(this.settings.classNames.sectionActive);
+					this.sectionButtons[i].addClass(this.settings.classNames.sectionButtonActive);
 				} else {
-					this.sectionPanels[i].removeClass('active');
-					this.sectionButtons[i].removeClass('active');
+					this.sectionPanels[i].removeClass(this.settings.classNames.sectionActive);
+					this.sectionButtons[i].removeClass(this.settings.classNames.sectionButtonActive);
 				}
 			}
 		}
